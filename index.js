92ce5b62ba75378529c32ce02defcd9790778e82
4512yasir <yasiryaska1@gmail.com>
2025-03-30 14:46:59 +0300

    OOP class

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here
+
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
+  
+  // Define the Lunch class
+  class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+  
+  // Define the Dinner class
+  class Dinner {
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      this.dessert = dessert;
+    }
+  }
+  
+  // Export classes for testing
+  module.exports = { Breakfast, Lunch, Dinner };
+  
